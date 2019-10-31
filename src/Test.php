d7bff747412fe79ee96bc8309bcaa94f8d4bbc9b
yhuaminghui <yhuaminghui@sina.com>
2019-10-31 16:43:31 +0800

    init

---
+++ src/Test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: huaminghui
+ * Date: 2019/10/31
+ * Time: 4:37 PM
+ */
+
+namespace Jushuitan;
+
+
+class Test
+{
+    public function index()
+    {
+        echo 'test package';
+    }
+}
